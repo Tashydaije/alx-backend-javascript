01fcdc8705c579bdb069ee14297202434d03fde5
Tashydaije <mbuguanatasha01@gmail.com>
2024-05-29 21:29:47 +0000

    fix eslint err remove unused func reject

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,5 +1,3 @@
-export default function getResponseFromAPI(){
-  return new Promise((resolve, reject) => {
-    resolve('success');
-  });
+export default function getResponseFromAPI() {
+  return Promise.resolve('success');
 }
