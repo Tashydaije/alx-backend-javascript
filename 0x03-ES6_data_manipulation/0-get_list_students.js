8831a31d4473f864c055eef3f27686afaec95af6
Tashydaije <mbuguanatasha01@gmail.com>
2024-06-04 11:49:48 +0000

    task0: func that returns an array object

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,8 @@
+export default function getListStudents() {
+  const array = [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Colombia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+  return array;
+}
