5638e91fad76d679afcdb48d9dd38d287d979e98
Tashydaije <mbuguanatasha01@gmail.com>
2024-06-04 15:35:37 +0000

    task3: using reduce on array

---
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,7 @@
+export default function getStudentIdsSum(students) {
+  if (!Array.isArray(students)) {
+    return [];
+  }
+
+  return students.reduce((accumulator, student) => accumulator + student.id, 0);
+}
