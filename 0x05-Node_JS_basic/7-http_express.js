f5ffd07823c290ad645f0445e225808f198b4041
Tashydaije <mbuguanatasha01@gmail.com>
2024-08-04 15:26:12 -0700

    refactor

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -5,20 +5,18 @@ const PORT = 1245;
 const app = express();
 
 app.get('/', (req, res) => {
-  res.status(200).contentType('text/plain').end('Hello Holberton School!');
+  res.status(200).contentType('text/plain').send('Hello Holberton School!');
 });
 
 app.get('/students', async (req, res) => {
   let message = 'This is the list of our students\n';
-  countStudents(process.argv[process.argv.length - 1])
-    .then((data) => {
-      message += data;
-      res.status(200).contentType('text/plain').end(message);
-    })
-    .catch((error) => {
-      console.error(error);
-      res.contentType('text/plain').end(error ? error.message : '');
-    });
+  try {
+    const data = await countStudents(process.argv[2]);
+    message += data;
+    res.status(200).contentType('text/plain').send(message);
+  } catch (error) {
+    res.status(500).contentType('text/plain').send('Cannot load the database');
+  }
 });
 
 app.listen(PORT);
