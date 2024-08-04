4fc477ad8ed2ba28f3ddeb22df0d2d34d83cc2f3
Tashydaije <mbuguanatasha01@gmail.com>
2024-08-04 14:48:49 -0700

    fix

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -11,12 +11,18 @@ const app = http.createServer(async (request, response) => {
     case '/students':
       message = 'This is the list of our students\n';
       try {
-        message += await countStudents(process.argv[process.argv.length - 1]);
+        const studentMessage = await countStudents(process.argv[2]);
+        message += studentMessage;
+        response.writeHead(200, { 'Content-Type': 'text/plain' });
       } catch (error) {
-        message = '';
+        message = 'Cannot load the database';
+        response.writeHead(500, { 'Content-Type': 'text/plain' });
       }
+      response.end(message);
       break;
     default:
+      response.writeHead(404, { 'Content-Type': 'text/plain' });
+      response.end('Not Found');
       break;
   }
   response.writeHead(200, { 'Content-Type': 'text/plain' });
